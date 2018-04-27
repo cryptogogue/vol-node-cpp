94ed997a14095222929d65ec99684e0ee40c07f3
Patrick Meehan <patrick@ziplinegames.com>
2018-04-27 16:34:47 -0700

    Initial Commit

---
+++ consensus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  consensus
+//
+//  Created by Patrick Meehan on 4/27/18.
+//  Copyright © 2018 Patrick Meehan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
