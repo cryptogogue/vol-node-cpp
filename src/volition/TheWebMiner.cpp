522d2fb48942ce77719099aed3d6c51594566725
Patrick Meehan <patrick@ziplinegames.com>
2018-07-10 19:44:59 -0700

    refactor - moved web miner api handlers into TheWebMinerAPI header and namespace

--- src/volition/TheWebMiner.cpp
+++ src/volition/TheWebMiner.cpp
@@ -1,146 +1,10 @@
 // Copyright (c) 2017-2018 Cryptogogue, Inc. All Rights Reserved.
 // http://cryptogogue.com
 
-#include <volition/AbstractAPIRequestHandler.h>
-#include <volition/TheTransactionFactory.h>
+#include <volition/TheWebMinerAPI.h>
 #include <volition/TheWebMiner.h>
 
 namespace Volition {
-
-//================================================================//
-// AccountDetailsHandler
-//================================================================//
-class AccountDetailsHandler :
-    public AbstractAPIRequestHandler {
-public:
-
-    SUPPORTED_HTTP_METHODS ( HTTP_GET )
-
-    //----------------------------------------------------------------//
-    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
-    
-        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
-    }
-};
-
-//================================================================//
-// AccountKeysHandler
-//================================================================//
-class AccountKeysHandler :
-    public AbstractAPIRequestHandler {
-public:
-
-    SUPPORTED_HTTP_METHODS ( HTTP_GET )
-
-    //----------------------------------------------------------------//
-    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
-    
-        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
-    }
-};
-
-//================================================================//
-// BlockDetailsHandler
-//================================================================//
-class BlockDetailsHandler :
-    public AbstractAPIRequestHandler {
-public:
-
-    SUPPORTED_HTTP_METHODS ( HTTP_GET )
-
-    //----------------------------------------------------------------//
-    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
-    
-        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
-    }
-};
-
-//================================================================//
-// BlockListHandler
-//================================================================//
-class BlockListHandler :
-    public AbstractAPIRequestHandler {
-public:
-
-    SUPPORTED_HTTP_METHODS ( HTTP_GET )
-
-    //----------------------------------------------------------------//
-    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
-        
-        const Chain* chain = TheWebMiner::get ().getChain ();
-        if ( chain ) {
-            jsonOut = chain->toJSON ();
-        }
-        return Poco::Net::HTTPResponse::HTTP_OK;
-    }
-};
-
-//================================================================//
-// DefaultHandler
-//================================================================//
-class DefaultHandler :
-    public AbstractAPIRequestHandler {
-public:
-
-    SUPPORTED_HTTP_METHODS ( HTTP_ALL )
-
-    //----------------------------------------------------------------//
-    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
-    
-        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
-    }
-};
-
-//================================================================//
-// MinerDetailsHandler
-//================================================================//
-class MinerDetailsHandler :
-    public AbstractAPIRequestHandler {
-public:
-
-    SUPPORTED_HTTP_METHODS ( HTTP_GET )
-
-    //----------------------------------------------------------------//
-    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
-    
-        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
-    }
-};
-
-//================================================================//
-// MinerListHandler
-//================================================================//
-class MinerListHandler :
-    public AbstractAPIRequestHandler {
-public:
-
-    SUPPORTED_HTTP_METHODS ( HTTP_GET )
-
-    //----------------------------------------------------------------//
-    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
-    
-         return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
-    }
-};
-
-//================================================================//
-// TransactionHandler
-//================================================================//
-class TransactionHandler :
-    public AbstractAPIRequestHandler {
-public:
-
-    SUPPORTED_HTTP_METHODS ( HTTP_POST )
-
-    //----------------------------------------------------------------//
-    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
-
-        unique_ptr < AbstractTransaction > transaction ( TheTransactionFactory::get ().create ( *jsonIn ));
-        TheWebMiner::get ().pushTransaction ( transaction );
-        
-        return Poco::Net::HTTPResponse::HTTP_OK;
-    }
-};
 
 //================================================================//
 // TheWebMiner
@@ -149,15 +13,15 @@ public:
 //----------------------------------------------------------------//
 TheWebMiner::TheWebMiner () {
     
-    this->mRouteTable.addEndpoint < AccountDetailsHandler >     ( "/accounts/:accountName/?" );         // GET
-    this->mRouteTable.addEndpoint < AccountKeysHandler >        ( "/accounts/:accountName/keys/?" );    // GET
-    this->mRouteTable.addEndpoint < BlockDetailsHandler >       ( "/blocks/:blockID/?" );               // GET
-    this->mRouteTable.addEndpoint < BlockListHandler >          ( "/blocks/?" );                        // GET
-    this->mRouteTable.addEndpoint < MinerDetailsHandler >       ( "/miners/:minerID/?" );               // GET
-    this->mRouteTable.addEndpoint < MinerListHandler >          ( "/miners/?" );                        // GET
-    this->mRouteTable.addEndpoint < TransactionHandler >        ( "/transactions/?" );                  // POST
+    this->mRouteTable.addEndpoint < TheWebMinerAPI::AccountDetailsHandler >     ( "/accounts/:accountName/?" );         // GET
+    this->mRouteTable.addEndpoint < TheWebMinerAPI::AccountKeysHandler >        ( "/accounts/:accountName/keys/?" );    // GET
+    this->mRouteTable.addEndpoint < TheWebMinerAPI::BlockDetailsHandler >       ( "/blocks/:blockID/?" );               // GET
+    this->mRouteTable.addEndpoint < TheWebMinerAPI::BlockListHandler >          ( "/blocks/?" );                        // GET
+    this->mRouteTable.addEndpoint < TheWebMinerAPI::MinerDetailsHandler >       ( "/miners/:minerID/?" );               // GET
+    this->mRouteTable.addEndpoint < TheWebMinerAPI::MinerListHandler >          ( "/miners/?" );                        // GET
+    this->mRouteTable.addEndpoint < TheWebMinerAPI::TransactionHandler >        ( "/transactions/?" );                  // POST
 
-    this->mRouteTable.setDefault < DefaultHandler >             ();
+    this->mRouteTable.setDefault < TheWebMinerAPI::DefaultHandler >             ();
 }
 
 //================================================================//
---
+++ src/volition/TheWebMinerAPI.h
@@ -0,0 +1,151 @@
+// Copyright (c) 2017-2018 Cryptogogue, Inc. All Rights Reserved.
+// http://cryptogogue.com
+
+#ifndef VOLITION_WEBMINERAPI_H
+#define VOLITION_WEBMINERAPI_H
+
+#include <volition/AbstractAPIRequestHandler.h>
+#include <volition/TheTransactionFactory.h>
+#include <volition/TheWebMiner.h>
+
+namespace Volition {
+namespace TheWebMinerAPI {
+
+//================================================================//
+// AccountDetailsHandler
+//================================================================//
+class AccountDetailsHandler :
+    public AbstractAPIRequestHandler {
+public:
+
+    SUPPORTED_HTTP_METHODS ( HTTP_GET )
+
+    //----------------------------------------------------------------//
+    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
+    
+        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
+    }
+};
+
+//================================================================//
+// AccountKeysHandler
+//================================================================//
+class AccountKeysHandler :
+    public AbstractAPIRequestHandler {
+public:
+
+    SUPPORTED_HTTP_METHODS ( HTTP_GET )
+
+    //----------------------------------------------------------------//
+    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
+    
+        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
+    }
+};
+
+//================================================================//
+// BlockDetailsHandler
+//================================================================//
+class BlockDetailsHandler :
+    public AbstractAPIRequestHandler {
+public:
+
+    SUPPORTED_HTTP_METHODS ( HTTP_GET )
+
+    //----------------------------------------------------------------//
+    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
+    
+        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
+    }
+};
+
+//================================================================//
+// BlockListHandler
+//================================================================//
+class BlockListHandler :
+    public AbstractAPIRequestHandler {
+public:
+
+    SUPPORTED_HTTP_METHODS ( HTTP_GET )
+
+    //----------------------------------------------------------------//
+    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
+        
+        const Chain* chain = TheWebMiner::get ().getChain ();
+        if ( chain ) {
+            jsonOut = chain->toJSON ();
+        }
+        return Poco::Net::HTTPResponse::HTTP_OK;
+    }
+};
+
+//================================================================//
+// DefaultHandler
+//================================================================//
+class DefaultHandler :
+    public AbstractAPIRequestHandler {
+public:
+
+    SUPPORTED_HTTP_METHODS ( HTTP_ALL )
+
+    //----------------------------------------------------------------//
+    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
+    
+        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
+    }
+};
+
+//================================================================//
+// MinerDetailsHandler
+//================================================================//
+class MinerDetailsHandler :
+    public AbstractAPIRequestHandler {
+public:
+
+    SUPPORTED_HTTP_METHODS ( HTTP_GET )
+
+    //----------------------------------------------------------------//
+    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
+    
+        return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
+    }
+};
+
+//================================================================//
+// MinerListHandler
+//================================================================//
+class MinerListHandler :
+    public AbstractAPIRequestHandler {
+public:
+
+    SUPPORTED_HTTP_METHODS ( HTTP_GET )
+
+    //----------------------------------------------------------------//
+    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
+    
+         return Poco::Net::HTTPResponse::HTTP_NOT_FOUND;
+    }
+};
+
+//================================================================//
+// TransactionHandler
+//================================================================//
+class TransactionHandler :
+    public AbstractAPIRequestHandler {
+public:
+
+    SUPPORTED_HTTP_METHODS ( HTTP_POST )
+
+    //----------------------------------------------------------------//
+    HTTPStatus AbstractAPIRequestHandler_handleRequest ( int method, const Routing::PathMatch& match, const Poco::JSON::Object::Ptr jsonIn, Poco::JSON::Object::Ptr& jsonOut ) const override {
+
+        unique_ptr < AbstractTransaction > transaction ( TheTransactionFactory::get ().create ( *jsonIn ));
+        TheWebMiner::get ().pushTransaction ( transaction );
+        
+        return Poco::Net::HTTPResponse::HTTP_OK;
+    }
+};
+
+} // namespace TheWebMinerAPI
+} // namespace Volition
+#endif
