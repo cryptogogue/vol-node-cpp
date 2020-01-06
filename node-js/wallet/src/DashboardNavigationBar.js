a3060e31aeab8ccf9c20d6392b56d9dfd71fc345
Patrick Meehan <patrick@ziplinegames.com>
2020-01-06 05:49:47 -0800

    added warning to delete local storage

--- node-js/wallet/src/DashboardNavigationBar.js
+++ node-js/wallet/src/DashboardNavigationBar.js
@@ -2,12 +2,24 @@
 
 import { ChangePasswordModal }              from './ChangePasswordModal';
 import { NavigationBar }                    from './NavigationBar';
+import { WarnAndDeleteModal }               from './WarnAndDeleteModal';
 import { observer }                         from 'mobx-react';
 import React, { useState }                  from 'react';
 import { Redirect }                         from 'react-router';
 import { Link }                             from 'react-router-dom';
 import { Dropdown, Icon, Label, Menu }      from 'semantic-ui-react';
-  
+
+const STORAGE_DELETE_WARNING_0 = `
+    Deleting local storage will delete all private
+    keys. Be sure you have a backup or your private keys
+    will be lost forever. This cannot be undone.
+`;
+
+const STORAGE_DELETE_WARNING_1 = `
+    If you lose your private keys, your assets and accounts cannot ever
+    be recovered. By anyone. Do you understand?
+`;
+
 //================================================================//
 // DashboardNavigationBar
 //================================================================//
@@ -15,6 +27,17 @@ export const DashboardNavigationBar = observer (( props ) => {
 
     const { appState } = props;
     const [ changePasswordModalOpen, setChangePasswordModalOpen ] = useState ( false );
+
+    const deleteStorageModal = (
+        <WarnAndDeleteModal
+            trigger = {
+                <Dropdown.Item icon = "warning circle" text = 'Delete Local Storage'/>
+            }
+            warning0 = { STORAGE_DELETE_WARNING_0 }
+            warning1 = { STORAGE_DELETE_WARNING_1 }
+            onDelete = {() => { appState.deleteStorage ()}}
+        />
+    );
 
     return (
         <React.Fragment>
@@ -32,7 +55,7 @@ export const DashboardNavigationBar = observer (( props ) => {
                         <Dropdown.Menu>
                             <Dropdown.Item icon = "wrench"          text = 'Schema Util'            as = { Link } to = { `/util/schema` }/>
                             <Dropdown.Item icon = "lock"            text = 'Change Password'        onClick = {() => { setChangePasswordModalOpen ( true )}}/>
-                            <Dropdown.Item icon = "warning circle"  text = 'Delete Local Storage'   onClick = {() => { appState.deleteStorage ()}}/>
+                            { deleteStorageModal }
                         </Dropdown.Menu>
                     </Dropdown>
                 </Menu.Menu>
