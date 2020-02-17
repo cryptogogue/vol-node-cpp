5a75cd8f17cae46a3f0f413225573c696feae54e
Patrick Meehan <patrick@ziplinegames.com>
2020-02-16 16:36:52 -0800

    volition node ops (run-volition-solo)

--- vol-node-cpp/ops/run-volition-solo.sh
+++ vol-node-cpp/ops/run-volition-solo.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-{
-    docker network create --driver bridge volition || true
-} &> /dev/null
+PROJECT_HOME=$(cd $(dirname "${0}")/../ && pwd)
 
 docker run -d --rm --name volition-node \
-    --network="volition" \
+    --network="fallguy" \
     -p "9090:9090" \
-    -v ${PWD}:/var/lib/volition \
+    -v ${PROJECT_HOME}:/var/lib/volition \
     volition/node \
-    -p 9090 -s true -k /var/lib/volition/keys/key9090.priv.json -g /var/lib/volition/genesis --simple-recorder-folder /var/lib/volition/persist-chain
+    -p 9090 -i 60 -s true -k /var/lib/volition/keys/key9090.priv.json -g /var/lib/volition/genesis --simple-recorder-folder /var/lib/volition/persist-chain
