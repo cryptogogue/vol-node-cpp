32aff7d60806f2889f2c0fbeccccf7a4674c5a79
Patrick Meehan <patrick@ziplinegames.com>
2020-02-19 05:49:08 -0800

    removed vol-wallet-js and submodules

--- vol-wallet-js/ops/run-wallet-bash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm --name volition-wallet-bash \
-    -v ${PWD}:/var/lib/volition-wallet \
-    --entrypoint "bash" \
-    volition/wallet
--- vol-wallet-js/ops/run-wallet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -d --rm --name volition-wallet \
-    -p "9999:80" \
-    -v ${PWD}:/var/lib/volition-wallet \
-    volition/wallet
