c2af0fdda73f66c960e14a206b5b1611068cef6e
Patrick Meehan <patrick@ziplinegames.com>
2020-05-19 21:32:27 -0700

    cmake version header script doesn’t work in docker build; moved to ops script for now

--- vol-node-cpp/ops/build-dev.sh
+++ vol-node-cpp/ops/build-dev.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# build facet backend docker image
+PROJECT_HOME=$(cd $(dirname "${0}")/../ && pwd)
+
+source ${PROJECT_HOME}/ops/make-version-header.sh
 docker build -t volition/node-dev .
