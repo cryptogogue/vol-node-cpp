8d433ef3442ed7b3aced441529ecbccaef5902d0
Patrick Meehan <patrick@ziplinegames.com>
2020-05-19 21:45:02 -0700

    changed volition dev node port to 9091

--- vol-node-cpp/ops/run-volition-solo-dev.sh
+++ vol-node-cpp/ops/run-volition-solo-dev.sh
@@ -5,7 +5,7 @@ PROJECT_HOME=$(cd $(dirname "${0}")/../ && pwd)
 docker run -d --name volition-node-dev      \
     --restart unless-stopped                \
     --network="fallguy"                     \
-    -p "9090:9090"                          \
+    -p "9091:9091"                          \
     -v ${PROJECT_HOME}:/var/lib/volition    \
     volition/node-dev                       \
-    -p 9090 -i 1 -s true -k /var/lib/volition/.keys/root.priv.json -g /var/lib/volition/genesis --simple-recorder-folder /var/lib/volition/persist-chain
+    -p 9091 -i 1 -s true -k /var/lib/volition/.keys/root.priv.json -g /var/lib/volition/genesis --simple-recorder-folder /var/lib/volition/persist-chain
