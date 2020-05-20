00c53fbe8c5a0aee57c3b08ceba4337eb02d0ea5
Patrick Meehan <patrick@ziplinegames.com>
2020-05-19 20:30:28 -0700

    added ops/volition-node-dev.sh

---
+++ vol-node-cpp/ops/run-volition-solo-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PROJECT_HOME=$(cd $(dirname "${0}")/../ && pwd)
+
+docker run -d --name volition-node-dev      \
+    --restart unless-stopped                \
+    --network="fallguy"                     \
+    -p "9090:9090"                          \
+    -v ${PROJECT_HOME}:/var/lib/volition    \
+    volition/node                           \
+    -p 9090 -i 1 -s true -k /var/lib/volition/.keys/root.priv.json -g /var/lib/volition/genesis --simple-recorder-folder /var/lib/volition/persist-chain
