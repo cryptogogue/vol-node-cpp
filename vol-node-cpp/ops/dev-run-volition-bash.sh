7a5205bd7dde9188f7f87f83b494d5a63cdae8f5
Patrick Meehan <patrick@ziplinegames.com>
2020-05-27 20:05:01 -0700

    fixed run dev back script

--- vol-node-cpp/ops/dev-run-volition-bash.sh
+++ vol-node-cpp/ops/dev-run-volition-bash.sh
@@ -5,7 +5,7 @@ PROJECT_HOME=$(cd $(dirname "${0}")/../ && pwd)
 docker stop volition-node-dev
 docker rm volition-node-dev
 
-docker run -it --rm --name volition-node-bash-dev \
+docker run -it --rm --name volition-node-dev \
     --network="fallguy"                     \
     -p "9091:9091"                          \
     -v ${PROJECT_HOME}:/var/lib/volition \
