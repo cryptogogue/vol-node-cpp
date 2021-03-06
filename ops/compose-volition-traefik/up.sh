c0673832bd761d41b654a43bc3ffc4295191863f
root <root@localhost.localdomain>
2021-03-06 19:59:06 +0000

    fixing traefik deployment

--- ops/compose-volition-traefik/up.sh
+++ ops/compose-volition-traefik/up.sh
@@ -3,5 +3,5 @@
 SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pushd ${SCRIPT_HOME}
-    docker-compose --env-file ../.env up -d --build -V
+    docker-compose --env-file ../.env up -d
 popd
